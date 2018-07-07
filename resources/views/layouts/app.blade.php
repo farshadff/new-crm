dbe159c6e7bb4a9001019799e9506a0ec1c85c9f
Farshad <farshad.badiee@gmail.com>
2018-07-07 16:14:37 +0430

    homepage changed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,10 +17,11 @@
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
 
+
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body>
+<body class="handshake">
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
