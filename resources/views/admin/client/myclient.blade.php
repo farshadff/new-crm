9ebea6f869d8a99c2e984e1660efaab633a320e0
Farshad <farshad.badiee@gmail.com>
2018-07-17 17:42:24 +0430

    a

---
+++ resources/views/admin/client/myclient.blade.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: farshad
+ * Date: 5/23/2018
+ * Time: 12:26 AM
+ */
+?>
+
+
+@include('header');
+@include('sidebar');
+
+
+<div class="content-wrapper">
+
+    <!-- Content area -->
+    <div class="content">
+        <!-- Horizontal borders -->
+        <div class="panel panel-flat">
+            <div class="panel-heading">
+                <h5 class="panel-title">مخاطبین</h5>
+                <div class="heading-elements">
+                    <ul class="icons-list">
+                        <li><a data-action="collapse"></a></li>
+                        <li><a data-action="reload"></a></li>
+                        <li><a data-action="close"></a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="panel-body">
+
+            </div>
+
+            <div class="table-responsive">
+                <table class="table">
+                    <thead class="thead-dark">
+                    <tr class="border-double">
+                        <th scope="col">ردیف</th>
+                        <th scope="col">نام مشتری</th>
+                        <th scope="col">توضیحات مشتری</th>
+                        <th scope="col">ساخته شده در</th>
+                        @foreach($sellman->sellmanlist as $sellmans)
+                            <th scope="col">مسولین فروش</th>
+                        @endforeach
+                        <th scope="col">عملیات ها</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+
+                        <tr>
+                            <th scope="row">{{$client->id}}</th>
+                            <td><a href="/admin/client/{{$client->id}}">{{$client->title}}</a></td>
+                            <td>{{$client->description}}</td>
+                            <td>
+                                {{new Verta($client->created_at)}}
+                            </td>
+                            @foreach($sellman->sellmanlist as $sellmans)
+                                <td>
+                                    {{$sellmans->name}}
+                                </td>
+                                {{--<td>--}}
+                                {{--{{$product->pivot->product_quantity}}--}}
+                                {{--</td>--}}
+                            @endforeach
+                            <td>
+                                <div class="btn-group inline-display" role="group" aria-label="Basic example">
+                                    <a href="{{ URL::to('admin/client/' . $client->id . '/edit') }}">
+                                        <button type="button" class="btn btn-info">ویراییش</button>
+                                    </a>&nbsp;
+                                    <form class="inline-display" action="{{url('admin/client', [$client->id])}}" method="POST">
+                                        <input type="hidden" name="_method" value="DELETE">
+                                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                        <input type="submit" class="btn btn-danger" value="حذف"/>
+                                    </form>
+                                </div>
+                            </td>
+                        </tr>
+
+                    </tbody>
+                </table>
+
+            </div>
+        </div>
+    </div>
+    @include('footer');
+</div>
+@include('footer');
---
+++ resources/views/admin/products/show.blade.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: farshad
+ * Date: 5/23/2018
+ * Time: 12:27 AM
+ */
+?>
+@include('header');
+@include('sidebar');
+<div class="content-wrapper">
+
+    <!-- Content area -->
+    <div class="content">
+        <!-- Horizontal borders -->
+        <div class="panel panel-flat border-left-info">
+            <div class="panel-heading">
+                <h5 class="panel-title">دفترچه تلفن</h5>
+                <div class="heading-elements">
+                    <ul class="icons-list">
+                        <li><a data-action="collapse"></a></li>
+                        <li><a data-action="reload"></a></li>
+                        <li><a data-action="close"></a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="panel-body">
+                <div class="table-responsive">
+                    <table class="table ">
+                        <thead >
+                        <tr class="border-double">
+                            <th>ردیف</th>
+                            <th>نام دسته</th>
+                            <th>توضیحات</th>
+                            <th >عملیات ها</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+
+                        <tr>
+                            <th scope="row">{{$productCat->id}}</th>
+                            <th scope="row">{{$productCat->title}}</th>
+                            <td><a href="/admin/product_cats/{{$productCat->id}}">{{$productCat->description}}</a></td>
+                            <td>
+                                <div class="btn-group inline-display" role="group" aria-label="Basic example">
+                                    <a href="{{ URL::to('admin/phonebooks/' . $productCat->id . '/edit') }}">
+                                        <button type="button" class="btn btn-success legitRipple">ویراییش</button>
+                                    </a>&nbsp;
+                                    <form class="inline-display"  action="{{url('admin/phonebooks', [$productCat->id])}}" method="POST">
+                                        <input type="hidden" name="_method" value="DELETE">
+                                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                        <input type="submit" class="btn btn-success legitRipple" value="حذف"/>
+                                    </form>
+                                </div>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+
+            <ul class="inline-list">
+                <li><a href=""><img src="http://atrisa-c0.ir/images/telegram.png" alt=""></a></li>
+                <li><a href=""><img src="http://atrisa-c0.ir/images/google.png" alt=""></a></li>
+                <li><a href=""><img src="http://atrisa-c0.ir/images/faceook.png" alt=""></a></li>
+                <li><a href=""><img src="http://atrisa-c0.ir/images/tiwtter.png" alt=""></a></li>
+            </ul>
+
+
+                </div>
+
+    </div>
+</div>
+@include('footer');
+
+
